2deb9d9a1870985bbc9ef8c85bfc62496b0565fa
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-06-19 08:42:40 +0200

    ft: add CR actions in service and controller

--- src/main/java/org/devkiki/langai/controller/ServiceItemController.java
+++ src/main/java/org/devkiki/langai/controller/ServiceItemController.java
@@ -1,25 +1,43 @@
 package org.devkiki.langai.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.devkiki.langai.dto.CreateServiceDto;
 import org.devkiki.langai.model.ServiceItem;
 import org.devkiki.langai.service.ServiceItemService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/services")
+@RequiredArgsConstructor
+@Tag(name = "Services Controller", description = "Endpoints to manage Services in H2 database")
 public class ServiceItemController {
     private final ServiceItemService service;
 
-    public ServiceItemController(ServiceItemService service) {
-        this.service = service;
+    @PostMapping
+    @Operation(summary = "Create a new service")
+    public ResponseEntity<ServiceItem> createServiceItem(@RequestBody CreateServiceDto dto) {
+        var createdService = service.create(dto);
+        return new ResponseEntity<>(createdService, HttpStatus.CREATED);
     }
 
-    @PostMapping
-    public ServiceItem createServiceItem(@RequestBody CreateServiceDto dto) {
-        return service.create(dto);
+    @GetMapping
+    @Operation(summary = "Get all services")
+    public ResponseEntity<List<ServiceItem>> getAllServiceItems() {
+        var services = service.all();
+        return new ResponseEntity<>(services, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get one service")
+    public ResponseEntity<ServiceItem> getServiceItem(@PathVariable Long id) {
+        var serviceItem = service.find(id);
+        return new ResponseEntity<>(serviceItem, HttpStatus.OK);
     }
 
 
--- src/main/java/org/devkiki/langai/service/ServiceItemService.java
+++ src/main/java/org/devkiki/langai/service/ServiceItemService.java
@@ -1,17 +1,17 @@
 package org.devkiki.langai.service;
 
+import lombok.RequiredArgsConstructor;
 import org.devkiki.langai.dto.CreateServiceDto;
 import org.devkiki.langai.model.ServiceItem;
 import org.devkiki.langai.repository.ServiceItemRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class ServiceItemService {
     private final ServiceItemRepository repository;
-
-    public ServiceItemService(final ServiceItemRepository repository) {
-        this.repository = repository;
-    }
 
     public ServiceItem create(CreateServiceDto dto) {
         ServiceItem serviceItem = new ServiceItem();
@@ -20,6 +20,15 @@ public class ServiceItemService {
         serviceItem.setPrice(dto.price());
 
         return repository.save(serviceItem);
+    }
+
+
+    public List<ServiceItem> all () {
+        return repository.findAll();
+    }
+
+    public ServiceItem find (Long id) {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Item not found!"));
     }
 
 
