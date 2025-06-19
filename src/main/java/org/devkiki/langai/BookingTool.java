4d6fb0d6897e9be2d655c3d9f4ab54c83ea8f853
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-06-19 08:43:11 +0200

    updated dependency injection and used lombok required args

--- src/main/java/org/devkiki/langai/BookingTool.java
+++ src/main/java/org/devkiki/langai/BookingTool.java
@@ -1,34 +1,32 @@
 package org.devkiki.langai;
 
 import dev.langchain4j.agent.tool.Tool;
+import lombok.RequiredArgsConstructor;
 import org.devkiki.langai.model.Booking;
 import org.devkiki.langai.model.ServiceItem;
 import org.devkiki.langai.repository.BookingRepository;
-import org.devkiki.langai.repository.ServiceItemRepository;
+import org.devkiki.langai.service.ServiceItemService;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class BookingTool {
-    private final ServiceItemRepository serviceItemRepository;
+    private final ServiceItemService serviceItemService;
     private final BookingRepository  bookingRepository;
 
-    public BookingTool(ServiceItemRepository serviceItemRepository, BookingRepository bookingRepository) {
-        this.serviceItemRepository = serviceItemRepository;
-        this.bookingRepository = bookingRepository;
-    }
 
     @Tool("List all available services for booking")
     public List<ServiceItem> findAll() {
-        return serviceItemRepository.findAll();
+        return serviceItemService.all();
     }
 
     @Tool("Book a service for a user at a given time")
     public Booking bookService(Long serviceId, String fullName, String email, String time) {
         LocalDateTime bookingTime = LocalDateTime.parse(time);
-        ServiceItem service = serviceItemRepository.findById(serviceId).orElseThrow();
+        ServiceItem service = serviceItemService.find(serviceId);
         Booking booking = new Booking();
         booking.setFullName(fullName);
         booking.setEmail(email);
--- src/main/java/org/devkiki/langai/controller/ChatController.java
+++ src/main/java/org/devkiki/langai/controller/ChatController.java
@@ -1,5 +1,8 @@
 package org.devkiki.langai.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.devkiki.langai.BookingAssistant;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,14 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/ai")
+@RequiredArgsConstructor
+@Tag(name = "Ai Chat Controller", description = "Endpoint to interact with Agentic RAG")
 public class ChatController {
     private final BookingAssistant assistant;
 
-    public ChatController(BookingAssistant assistant) {
-        this.assistant = assistant;
-    }
-
     @PostMapping("/chat")
+    @Operation(summary = "Interact with Agentic RAG - Just prompt")
     public ResponseEntity<String> chat(@RequestBody ChatRequest chatRequest) {
         String message = chatRequest.message();
         String response = assistant.chat(message);
